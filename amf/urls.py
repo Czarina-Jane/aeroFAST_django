951536f4273bb45d3b53075b308d3cd74a2bfa04
czarina_rosales <czarina.rosales1090@gmail.com>
2024-04-15 14:20:12 +0800

    from .views import * change to this code

--- amf/urls.py
+++ amf/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import amf, fbph, taf, sigmet, others # Add home function from views
+# pinalitan ko ng * para lahat pre ng nasa views.py mo macall mo dito
+
+from .views import * # Add home function from views
 
 urlpatterns = [    
     # Add path from local views
     path('amf/', amf, name='amf'), 
-    path('amf/fbph/', fbph, name='fbph'), 
     path('amf/taf/', taf, name='taf'), 
     path('amf/sigmet/', sigmet, name='sigmet'),
     path('amf/others/', others, name='others'),  
+    path('amf/fbph', fbph, name='fbph'),
 ]

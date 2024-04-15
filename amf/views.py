29fff2421d087b036db0b372f4ac32b0fa20180e
czarina_rosales <czarina.rosales1090@gmail.com>
2024-04-15 14:30:31 +0800

    change function of FBPH

--- amf/views.py
+++ amf/views.py
@@ -4,6 +4,15 @@ def amf(request):
     return render(request, 'amf.html')
 
 def fbph(request):
+    # yung pOST yan yung method sa form mo. kinuha niya na yung data sa form mo
+    if request.method == 'POST':
+        SYN = request.POST.get('SYN')
+        PHILAWYS = request.POST.get('PHILAWYS')
+        #  test lang sa terminal kung may nakukuha na data. pwede idelete
+        print("SYN:", SYN)
+        print("PHILAWYS:", PHILAWYS)
+        # nilagay lang sa array yung data para ito yung icall sa preview.html
+        return render(request, 'preview.html', {'SYN': SYN, 'PHILAWYS': PHILAWYS})
     return render(request, 'amf/fbph.html')
 
 def taf(request):
